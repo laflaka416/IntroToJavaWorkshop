5b82c3fda4bf285ec1011f2d37cc6d1fc457e86e
laflaka416 <league@Mac-mini-3.local>
2016-08-15 12:02:23 -0700

    Kiritoday1

--- src/day1/robot/MyFirstJavaProgram.java
+++ src/day1/robot/MyFirstJavaProgram.java
@@ -7,9 +7,47 @@ public class MyFirstJavaProgram {
 	public static void main(String[] args) {
 		
 		// START HERE
+Robot Kirito = new Robot();
+Kirito.penDown();
+for (int i = 0; i < 4; i++) {
+Kirito.move(100);
+Kirito
+}
 
-	
-		
-		
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Kirito.penDown();
+//Kirito.setPenColor(255, 017, 000);
+//Kirito.move(100);
+//Kirito.turn(90);
+//Kirito.setPenColor(000, 051, 221);
+//Kirito.move(100);
+//Kirito.turn(90);
+//Kirito.setPenColor(255, 017, 000);
+//Kirito.move(100);
+//Kirito.turn(90);
+//Kirito.setPenColor(000, 051, 221);
+//Kirito.move(100);
+//Kirito.hide();
+
 	}
 }
--- src/day1/robot/PentagonCrazy.java
+++ src/day1/robot/PentagonCrazy.java
@@ -9,26 +9,26 @@ import org.jointheleague.graphical.robot.Robot;
 public class PentagonCrazy {
 
 	private void makePrettyThings() {
-		// 1. Create a new Robot
+Robot Kirito = new Robot();	
 
-		// 3. Put the robot's pen down
 
+Kirito.penDown();
 		// 8. Make the robot go at maximum speed (10)
 
 		// 9. Set the pen to a color that you like for the shape
 
-		// 4. Make a variable for the number of sides you want (can’t test this one)
 
-		// 5. Make a variable for the angle you want the robot to turn. Hint: you can divide in Java using "/". Can’t test until step 6.
-
+Integer sides = 5;
+		
+Integer angle = 360/sides;
 		// 7. Do steps #2 to #11, 200 times. When this is done you should see a pentagon.
 
-			// 2. Move the robot 200 pixels
-	
+
+Kirito.move(200);	
 			// 10. Make the robot move "i" pixels instead of 200 (don’t need new line of code for this, just change previous one)
 	
 			// 6. Turn the robot the amount in your angle variable
-	
+Kirito.turn(360/sides);	
 			// 11. Turn the robot one more degree
 
 	}
